2388311854bd75ff64b85a9ccbb65c085e8755c2
Руслан <ryslan.shuyski@mail.ru>
2020-10-11 20:47:02 +0300

    3. Арифметические операции. [#383682]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,7 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("10.10.2020");
-
+        System.out.println("Сегодня я молодец!");
     }
 }
