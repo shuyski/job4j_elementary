af49988b38357de742e15f95f98f2c8f7038fa1c
Руслан <ryslan.shuyski@mail.ru>
2020-10-14 22:43:16 +0300

    3.4. Заголовок в файле java [#383691]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,7 +1,8 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args) {
-        System.out.println("Сегодня я молодец!");
+    public static void main(String[] arfs) {
+        System.out.println("10.10.2020");
+
     }
 }
