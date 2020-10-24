ec39a3693b47850c3eecefc31fc56a7029eae553
Руслан <ryslan.shuyski@mail.ru>
2020-10-24 15:47:16 +0300

    6.0.2. Заполнение массива. [#383651]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,21 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
+       /* short[] ages = new short[10];
         String[] surnames = new String[10050];
         float[] prices = new float[40];
         System.out.println("Размер массива ages равен : " + ages.length);
         System.out.println("Размер массива surnames равен : " + surnames.length);
-        System.out.println("Размер массива prices равен : " + prices.length);
+        System.out.println("Размер массива prices равен : " + prices.length); */
+        String[] names = new String[4];
+        names[0] = "Шуйский Руслан";
+        names[1] = "Korobeinikov Stas";
+        names[2] = "Sergey Mayer";
+        names[3] = "Andrei Hincu";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
