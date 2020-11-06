e7d1f457df907482bff7601357da65213e510b78
Руслан <ryslan.shuyski@mail.ru>
2020-11-06 14:10:25 +0300

    2.1. Рефакторинг - Расстояние между точками. [#383826]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,13 +1,26 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        double rsl = sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
         return rsl;
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(12, 3, 14, 5);
+        Point one = new Point(12, 14);
+        Point two = new Point(3, 5);
+        double result = one.distance(two);
         System.out.println("result (12, 14) to (3, 5) = " + result);
     }
 }
